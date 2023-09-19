773f14a77eebdd34aa958bd84a14146e23d5e6bd
Zhakiya-00 <zakiyayusuf123@gmail.com>
2023-09-19 12:30:09 +0300

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module for trying out Babel i18n """
-from flask_babel import Babel
-from flask import Flask, render_template
+from flask_babel import Babel, _
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
@@ -21,14 +21,16 @@ app.config.from_object(Config)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world() -> str:
     """Renders a Basic Template for Babel Implementation"""
-    return render_template("2-index.html")
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale() -> str:
     """Select a language translation to use for that request"""
+    locale = request.args.get("locale")
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run()
